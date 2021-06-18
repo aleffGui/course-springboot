3da79d1fd92d6098c752b3cc6be160cdd310c137
aleffGui <alefff.guilherme@gmail.com>
2021-06-18 11:20:38 -0300

    Service layer, component registration

--- src/main/java/com/courseapp/course/resources/UserResource.java
+++ src/main/java/com/courseapp/course/resources/UserResource.java
@@ -1,18 +1,30 @@
 package com.courseapp.course.resources;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.courseapp.course.entities.User;
+import com.courseapp.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	@Autowired
+	private UserService us;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> users = us.findAll();
+		return ResponseEntity.ok().body(users);
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable("id") long id) {
+		User user = us.findById(id);
+		return ResponseEntity.ok().body(user);	
 	}
 }
---
+++ src/main/java/com/courseapp/course/services/UserService.java
@@ -0,0 +1,23 @@
+package com.courseapp.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.courseapp.course.entities.User;
+import com.courseapp.course.repositories.UserRepository;
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository ur;
+	
+	public List<User> findAll() {
+		return ur.findAll();
+	}
+	public User findById(long id) {
+		Optional<User> user = ur.findById(id);
+		return user.get();
+	}
+}
